72a256960fdec21b6d275faafc2006b427f99596
Timofey <froepamtima@gmail.com>
2021-05-19 23:18:57 +0300

    User created

---
+++ src/bsu/rfe/java/group_8/lab_7/kononovich/var_b_3/User.java
@@ -0,0 +1,19 @@
+package bsu.rfe.java.group_8.lab_7.kononovich.var_b_3;
+
+
+public class User {
+    private String name;
+    private String addres;
+
+    public User (String name, String addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getAddres(){
+        return addres;
+    }
+}
